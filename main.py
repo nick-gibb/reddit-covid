e79c43717389084b39f527de03bcc14f18cbec5f
John Kim <12468676+John3Kim@users.noreply.github.com>
2020-09-28 16:17:21 -0400

    Update main.py
    Make it less busy by modularizing the code better (i.e. moved loops outside of the export_submissions function)
    Added datatypes to the functions and default types

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
 from datetime import date, timedelta
+import datetime
 from pprint import pprint
 import json
 import requests as r
 import time
 
 
-def make_date_range(sdate, edate):
-    delta = edate - sdate
+def make_date_range(start_date:datetime.timedelta, end_date: datetime.timedelta) -> datetime.timedelta:
+    delta = end_date - start_date
     timestamps = []
 
     for i in range(delta.days + 1):
-        day = sdate + timedelta(days=i)
+        day = start_date + timedelta(days=i)
         unix_timestamp = str(int(time.mktime(day.timetuple())))
         timestamps.append(unix_timestamp)
+
     return timestamps
 
 
-def export_submissions(subreddits, queries, timestamps):
+def export_submissions(subreddit:str, query:str, timestamps:datetime.timedelta, num_retries:int=1, file_export_name:str="data") -> None:
 
-    file_export_name = "data2"
+    for i in range(len(timestamps)):
+        
+        for num_retry in range(num_retries+1):
+
+            try:
+                query = "covid"
+
+                if i == len(timestamps)-1:
+                    break
+
+                after = timestamps[i]
+                before = timestamps[i+1]
+                api_call = f'https://api.pushshift.io/reddit/search/submission/?q={query}&subreddit={subreddit}&after={after}&before={before}&limit=500'
+                
+                time.sleep(1.2)
+
+                req = r.get(api_call)
+
+                submission = req.json()['data'] 
+
+                with open(f"{file_export_name}.json", 'a', encoding='utf-8') as f:
+                    json.dump(submission, f, ensure_ascii=False, indent=4)
+                
+                print("Data exported to {name}".format(name=file_export_name))
+
+            except json.decoder.JSONDecodeError:
+                print("Failed to parse: {api_call}")
+                print("Adding url to an errors text file") 
+
+                with open(f"{file_export_name}_error_url.txt", 'a', encoding='utf-8') as error_file:
+                    error_file.write("{api_call}\n")
+                
+                print("Done adding url to errors.txt file.  Continuing on...") 
+                continue 
+
+            break
+
+
+def main() -> None:
+    #edate = date.today()
+    start_date = date(2020, 4, 1)
+    end_date = date(2020, 4, 30)
+    timestamps = make_date_range(start_date, end_date)
+    
+    queries=['covid', 'coronavirus','sars-cov-2']
+    subreddits=['Canada', 'CanadaPolitics', 'CanadaCoronavirus', 
+                'Vancouver', 'Edmonton', 'Winnipeg', 
+                'Montreal', 'Ottawa', 'Saskatoon', 
+                'Calgary', 'Toronto', 'Ontario', "onguardforthee"]
 
     for query in queries:
         print(f'Query: {query}')
@@ -26,47 +76,7 @@ def export_submissions(subreddits, queries, timestamps):
         for subreddit in subreddits:
             print(f'Subreddit: {subreddit}')
 
-            for i in range(len(timestamps)):
-                
-                try:
-                    query = "covid"
-
-                    if i == len(timestamps)-1:
-                        break
-
-                    after = timestamps[i]
-                    before = timestamps[i+1]
-                    api_call = f'https://api.pushshift.io/reddit/search/submission/?q={query}&subreddit={subreddit}&after={after}&before={before}&limit=500'
-                    
-                    time.sleep(1.2)
-
-                    req = r.get(api_call)
-
-                    submission = req.json()['data'] 
-
-                    with open(f"{file_export_name}.json", 'a', encoding='utf-8') as f:
-                        json.dump(submission, f, ensure_ascii=False, indent=4)
-                    
-                    print("Data exported to {name}".format(name=file_export_name))
-
-                except json.decoder.JSONDecodeError:
-                    print("Failed to parse: {api_call}")
-                    print("Adding url to an errors text file") 
-
-                    with open(f"{file_export_name}_error_url.txt", 'a', encoding='utf-8') as error_file:
-                        error_file.write("{api_call}\n")
-                    
-                    print("Done adding url to errors.txt file.  Continuing on...") 
-                    continue
-
-
-def main():
-    #edate = date.today()
-    sdate = date(2020, 4, 1)
-    edate = date(2020, 4, 30)
-    timestamps = make_date_range(sdate, edate)
-    export_submissions(subreddits=['Canada', 'CanadaPolitics', 'CanadaCoronavirus', 'Vancouver', 'Edmonton', 'Winnipeg', 'Montreal', 'Ottawa', 'Saskatoon', 'Calgary', 'Toronto', 'Ontario'], queries=[
-                                 'covid', 'coronavirus','sars-cov-2'], timestamps=timestamps)
+            export_submissions(subreddit=subreddit, query=query, timestamps=timestamps)
 
 if __name__ == "__main__":
     main()
