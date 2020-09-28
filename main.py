6484d1a509d726fa07f7f56cefaf01a3eb3b3975
John Kim <12468676+John3Kim@users.noreply.github.com>
2020-09-28 15:35:21 -0400

    Shorter Code
    Append instead of write to file so that we don't have too much data in memory

--- main.py
+++ main.py
@@ -16,63 +16,57 @@ def make_date_range(sdate, edate):
     return timestamps
 
 
-def get_submissions(subreddits, queries, timestamps):
+def export_submissions(subreddits, queries, timestamps):
 
-    submissions = []
+    file_export_name = "data2"
+
     for query in queries:
         print(f'Query: {query}')
+
         for subreddit in subreddits:
-            total_comments = 0
             print(f'Subreddit: {subreddit}')
-            submission_count = 0
+
             for i in range(len(timestamps)):
-                query = "covid"
-                if i == len(timestamps)-1:
-                    break
-                after = timestamps[i]
-                before = timestamps[i+1]
-                api_call = f'https://api.pushshift.io/reddit/search/submission/?q={query}&subreddit={subreddit}&after={after}&before={before}&limit=500'
-                # time.sleep(1)
-                req = r.get(api_call)
+                
+                try:
+                    query = "covid"
 
-                if 'json' in req.headers.get('Content-Type'):
-                    resp = req.json()['data']
-                    submission_count += len(resp)
-                else:
-                    pprint(api_call)
-                    pprint(req.headers.get('Content-Type'))
+                    if i == len(timestamps)-1:
+                        break
 
-                    pprint(req.json())
-                    breakpoint
-                    # print('Response content is not in JSON format.')
+                    after = timestamps[i]
+                    before = timestamps[i+1]
+                    api_call = f'https://api.pushshift.io/reddit/search/submission/?q={query}&subreddit={subreddit}&after={after}&before={before}&limit=500'
+                    
+                    time.sleep(1.2)
+
+                    req = r.get(api_call)
+
+                    submission = req.json()['data'] 
+
+                    with open(f"{file_export_name}.json", 'a', encoding='utf-8') as f:
+                        json.dump(submission, f, ensure_ascii=False, indent=4)
+                    
+                    print("Data exported to {name}".format(name=file_export_name))
+
+                except json.decoder.JSONDecodeError:
+                    print("Failed to parse: {api_call}")
+                    print("Adding url to an errors text file") 
+
+                    with open(f"{file_export_name}_error_url.txt", 'a', encoding='utf-8') as error_file:
+                        error_file.write("{api_call}\n")
+                    
+                    print("Done adding url to errors.txt file.  Continuing on...") 
                     continue
-                for submission in resp:
-                    total_comments += submission['num_comments']
-                    # reduced_submission = {
-                    #     'full_link': submission['full_link'],
-                    #     'title': submission['title'],
-                    #     'num_comments': submission['num_comments'],
-                    #     'score': submission['score'],
-                    #     'created_utc': time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(submission['created_utc']))
-                    # }
-                    submissions.append(submission)
-            print(
-                f'Post submission count in /r/{subreddit} with query "{query}": {submission_count}')
-            print(
-                f'Comment count in /r/{subreddit} with query "{query}": {total_comments}')
-    return submissions
 
 
 def main():
+    #edate = date.today()
     sdate = date(2020, 4, 1)
-    # edate = date(2020, 9, 3)
-    edate = date.today()
+    edate = date(2020, 4, 30)
     timestamps = make_date_range(sdate, edate)
-    submissions = get_submissions(subreddits=['Canada', 'CanadaPolitics', 'CanadaCoronavirus', 'Vancouver', 'Edmonton', 'Winnipeg', 'Montreal', 'Ottawa', 'Saskatoon', 'Calgary', 'Toronto', 'Ontario'], queries=[
-                                  'covid', 'coronavirus'], timestamps=timestamps)
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(submissions, f, ensure_ascii=False, indent=4)
-
+    export_submissions(subreddits=['Canada', 'CanadaPolitics', 'CanadaCoronavirus', 'Vancouver', 'Edmonton', 'Winnipeg', 'Montreal', 'Ottawa', 'Saskatoon', 'Calgary', 'Toronto', 'Ontario'], queries=[
+                                 'covid', 'coronavirus','sars-cov-2'], timestamps=timestamps)
 
 if __name__ == "__main__":
     main()
